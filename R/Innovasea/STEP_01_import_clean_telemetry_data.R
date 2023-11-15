800fc5cda90466150c60b9fffe5e039fa7069b61
benjaminhlina <benjamin.hlina@gmail.com>
2023-11-15 08:41:42 -0500

    Fix formatting and codespace section

--- R/Innovasea/STEP_01_import_clean_telemetry_data.R
+++ R/Innovasea/STEP_01_import_clean_telemetry_data.R
@@ -37,11 +37,11 @@ dat <- map_df(file.list, read_csv, .id = "id") %>%
 glimpse(dat)
 
 dat
-# check that download group is unique convert id/download group to numeric -----
+# ---- Check that download group is unique convert id/download group to numeric -----
 unique(dat$id)
 dat$id  <- as.numeric(dat$id)
 
-# ----- drop blank columns -----
+# ---- Drop blank columns -----
 
 dat <- dat %>% 
   select(id:sensor_unit, -transmitter_name) %>% 
@@ -51,29 +51,31 @@ dat <- dat %>%
     detection_timestamp_utc = date_and_time_utc
   )
 
-# ---- bring in reciever location ----
+# ---- Bring in receiver location ----
 
 rl <- read_csv(here::here("Data", 
+                          "Innovasea",
                           "Metadata",
                           "receiver_metadata.csv")) %>%
   clean_names()
 
 glimpse(rl)
 
-# ----- bring in metadata for tagged fish -----
+# ---- Bring in metadata for tagged fish -----
 fish_tag_data <- read_csv(here::here("data", 
+                                     "Innovasea",
                                      "metadata",
                                      "fish_tagging_metadata.csv")) %>%
   clean_names()
 glimpse(fish_tag_data)
-# For this exmample we will just grab just lake trout -----
+# ---- For this example we will just grab just lake trout -----
 lt_tagged <- fish_tag_data %>%
   filter(species %in% "LT") 
 
 glimpse(lt_tagged)
 
 
-# remove any transmitter serial columns that have NA ------
+# ---- remove any transmitter serial columns that have NA ------
 
 lt_tagged <- lt_tagged %>% 
   filter(transmitter_serial != is.na(transmitter_serial))
@@ -83,7 +85,7 @@ lt_tagged <- lt_tagged %>%
   mutate(tag_date = dmy(tag_date))
 
 
-# ------ Filter out detections so only LKT are present ------
+# ---- Filter out detections so only LKT are present ------
 
 dat <- dat %>% 
   arrange(date_and_time_utc)
@@ -92,7 +94,7 @@ dat$transmitter_serial <- as.character(dat$transmitter_serial)
 
 
 
-# Remove dead fish  ----- 
+# ---- Remove Transmitters that were not in the system (False Detection) ----- 
 # remove false detections from whole dataframe and sort just for species
 
 
@@ -101,7 +103,7 @@ dat <- dat %>%
 
 glimpse(dat)
 
-# add in column that is transmitter code space ----- 
+# ---- Add in column that is transmitter code space ----- 
 
 dat$transmitter_codespace <- dat$transmitter_id
 
@@ -109,7 +111,7 @@ glimpse(dat)
 
 
 
-# remove dashes from both receiver and trasmitter columns ----
+# ---- remove dashes from both receiver and transmitter columns ----
 dat$receiver_sn <- dat$receiver_sn %>% 
   str_replace(".*-", "")
 
@@ -120,37 +122,40 @@ glimpse(dat)
 
 
 
-# if else statment to properly get codespaces -----
+# ---- if else statement to properly get codespaces -----
 # first create vector of the length of each string to test against 
 
 
-unique(dat$transmitter_codespace)
+
 
 cs_lgth <- str_length(dat$transmitter_codespace)
 unique(cs_lgth)
 # you need to see if the length of the code spaces differed for instance it could
 # be A69-9002-10683. The codespace is A69-9002. We need to drop the tag ID from
-# the codespace column. To do so we first neeed to determine the length of the 
-# string for each each full transmition. then we need to remove the last characters
+# the codespace column. To do so we first need to determine the length of the 
+# string for each each full transmission. then we need to remove the last characters
 # prior to the last dash and the dash. In the below example it was 5 or 6 
 # characters...I am not sure what your's will be but you will need to change 
-# 13 to the correct length of cs_lenght. If you have more than 2 cs_lgths, 
+# 13 to the correct length of cs_lgth). If you have more than 2 cs_lgths, 
 # we can write a case_when statement to do this. Depending on the number 
 # of characters you need to remove, change 5 and 6 below. 
 
 # use if_else to have it search for when the length of the code space
 # equals 13 characters and have it remove those last 6 characters 
 # then if it equals 13 have it only remove the last 5 characters 
-# 
 
+# for this example the code space is all the same so n need to 
 dat <- dat %>% 
   mutate(
-    transmitter_codespace =  .$transmitter_codespace %>%
-      str_replace(".{6}$", ""))
+    transmitter_codespace = str_replace(transmitter_codespace, ".{6}$", "")
+    )
 
 
-# need to join receiver meta data and fish metadata 
+# ---- join receiver metadata --------
+# ---- join fish metadata -------- 
 
-write_rds(dat, here("Saved Data", 
-                    "Cleaned Telemetry Data", 
+# ---- Once Clean export data -----
+write_rds(dat, here("Saved-Data", 
+                    "Innovasea",
+                    "Cleaned-Telemetry-Data", 
                     "cleaned_telemetry_file.rds"))
