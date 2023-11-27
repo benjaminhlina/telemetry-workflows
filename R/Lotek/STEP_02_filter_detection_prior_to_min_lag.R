bbd37c990eeb3db1cef1101d66c9683b29fe2d8c
benjaminhlina <benjamin.hlina@gmail.com>
2023-11-27 10:44:36 -0500

    Created filter of detections that were heard prior to tag deployment or did not have a hex ID

---
+++ R/Lotek/STEP_02_filter_detection_prior_to_min_lag.R
@@ -0,0 +1,38 @@
+# ---- load packages ----
+{
+  library(chron)
+  library(data.table)
+  library(dplyr)
+  library(here)
+  library(lubridate)
+  library(purrr)
+  library(readr)
+  library(stringr) 
+  library(tidyr)
+}
+# ---- bring in detection data ----
+
+dat <- read_rds(here("saved-data", 
+                     "Lotek-cleaned_telemetry-data", 
+                     "lotek_telemetry_joined_metadata.rds"))
+
+glimpse(dat)
+# ---- Check if tags were detected BEFORE they were deployed ----
+dat <- dat %>%
+  group_by(hex) %>%
+  arrange(date_time) %>%
+  mutate(
+    before_release = if_else(date_time < release_date,
+                             false = 1, true = 0)
+  ) %>% 
+  ungroup()
+
+# ----- filter out detections heard before deployed or didn't have a hex ----
+dat <- dat %>% 
+  filter(before_release == 1)
+
+# ---- Export out our filtered data ---- 
+
+write_rds(dat, here("saved-data", 
+               "Lotek-cleaned_telemetry-data", 
+               "lotek_telemetry_joined_metadata_filtered.rds"))
