88ad76909a8f2c73487c6c224d9e219615ed36f1
benjamin.hlina <benjamin.hlina@gmail.com>
2024-02-15 13:42:44 -0500

    Updated step 3

--- R/Lotek/STEP_03_min_lag_false_detections.R
+++ R/Lotek/STEP_03_min_lag_false_detections.R
@@ -11,6 +11,10 @@
   library(tidyr)
   source(here("functions", 
               "min_lag_lotek.R"))
+  source(here("functions", 
+              "false_detection_lotek.R"))
+  source(here("functions", 
+              "make_linestring.R"))
 }
 # ---- bring in detection data ----
 
@@ -42,24 +46,80 @@ glimpse(dat)
 glimpse(dat)
 
 
-
-t <- false_detections_lotek(dets = dat, end_min = 1200, end_max = 1220, 
-                            start_min = 15, start_max = 25, interval = 20)
-
-glimpse(t)
+# ---- interval filter ---- 
+dat <- false_detections_lotek(dets = dat)
 
 
-
-tes <- dat %>% 
-  group_by(hex) %>% 
-  arrange(date_time) %>% 
-  ungroup() %>% 
-  split(.$hex) %>% 
-  map(~ false_detections_lotek(dets = ., end_min = 1200, end_max = 1220, 
-                               start_min = 15, start_max = 25, interval = 20)) %>% 
-  bind_rows()
-
-glimpse(tes)
+# ---- rename ---- 
+dat <- dat %>% 
+  rename(
+    animal_id = hex,
+    detection_timestamp_utc = date_time,
+    deploy_lat = rec_lat, 
+    deploy_long = rec_long
+  ) 
 
 
+# ----- create detection events filter ----- 
+dtc <- detection_events(dat, 
+                        location_col = "station", 
+                        time_sep = 3600 * 1, 
+                        condense = TRUE) 
 
+dtc
+
+# ----- DO NOT UNCOMMENT AS THIS CODE IS IN DEVELOPMENT ------
+
+
+
+
+
+
+# View(dtc)
+# 
+# dtc2 <- dtc2[order(dtc2$animal_id, dtc2$first_detection),] 
+# #make sure to order the dataframe before conducting the distance filter 
+# glimpse(dtc)
+# View(dtc)
+# 
+# #speed filter
+# 
+# dtc2$dist <- as.numeric(NA)
+# dtc2$tstep <- as.numeric(NA) # variable for number of s from previous detection (on any receiver)
+# geosphere::distHaversine(c(-76.056, 44.892833), c(-76.026416, 44.896157)) 
+# #2362.144 - looking at distance between those two particular points we were interested in, draws a direct line so isn't quite the best but is decent 
+# View(dtc) #nothing in dist or tstep as of now, jsut a bunch of NAs
+# 
+# geosphere::distHaversine(c(-76.31968, 44.64725), c(-76.295572, 44.702990))
+# 
+# fish <- sort(unique(dtc$animal_id)) #49 fish made it past the filter 
+# length(unique(fish))
+# glimpse(dtc)
+# i <- 1 
+# for(i in 1:length(fish)){
+#   dtc2.i <- dtc[dtc$animal_id==fish[i],]
+#   
+#   if(nrow(dtc2.i)>1){
+#     llon <- as.vector(dtc2.i$mean_longitude)
+#     llat <- as.vector(dtc2.i$mean_latitude)
+#     lon <- append(NA, llon)
+#     lon <- as.vector(lon[-length(lon)])
+#     lat <- append(NA, llat)
+#     lat <- as.vector(lat[-length(lat)])
+#     t <- data.frame(lon, lat, llon, llat)
+#     
+#     t
+#     m <- make_line(lon = t$plon, lat = t$plat, llon = t$curlon, llat = t$curlat)
+#     dtc2.i$dist <- geosphere::distHaversine(cbind(plon, plat), cbind(curlon, curlat))
+#     
+#     cur_time <- as.vector(dtc2.i$first_detection)
+#     pt <- append(NA, as.vector(dtc2.i$last_detection))
+#     pt <- as.vector(pt[-length(pt)])
+#     dtc2.i$tstep <- cur_time-pt
+#     
+#     dtc2[row.names(dtc2.i),] <- dtc2.i
+#   }
+#   
+# }
+# 
+# 
